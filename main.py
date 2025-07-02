a7cdc59ca5e1441283b2d49ac8ba227a8c3aba68
Estudiante <pvazsquezpetracca@alumno.unlam.edu.ar>
2025-07-02 03:17:23 -0300

    Refactor Main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Box2D.b2 import world as b2World
-from Box2D import b2World, b2PolygonShape, b2_staticBody, b2_dynamicBody
+from Box2D import b2World, b2PolygonShape
 from sound_manager import SoundManager
 from screens.menu import main_menu_screen
 from screens.character_select import character_selection_screen
@@ -12,126 +12,186 @@ import IA_player
 import threading
 from timer import draw_timer, timer_thread
 
+class Game:
+    def __init__(self):
+        pygame.init()
 
-pygame.init()
-font = pygame.font.SysFont(None, 36)
-font_timer = pygame.font.SysFont("arialblack", 60)
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Pixel Fighter")
+        self.clock = pygame.time.Clock()
 
-TEXT_KO = pygame.font.SysFont(None, 36).render("KO", True, (200, 0, 0))
-X_KO = WIDTH // 2 - TEXT_KO.get_width() // 2
+        self.font = pygame.font.SysFont(None, 36)
+        self.font_timer = pygame.font.SysFont("arialblack", 60)
 
-X_BAR_P1 = X_KO - HEALTH_BAR_WIDTH - BAR_SPACING
-X_BAR_P2 = X_KO + TEXT_KO.get_width() + BAR_SPACING
-Y_BAR = Y_KO + 5
+    def draw_health_bars(self, surface, p1_healt, p2_healt):
+        prog_p1 = self.calculate_health_width(p1_healt, MAX_HP, HEALTH_BAR_WIDTH)
+        prog_p2 = self.calculate_health_width(p2_healt, MAX_HP, HEALTH_BAR_WIDTH)
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Pelea con Box2D")
+        TEXT_KO = pygame.font.SysFont(None, 36).render("KO", True, (200, 0, 0))
+        X_KO = WIDTH // 2 - TEXT_KO.get_width() // 2
+
+        X_BAR_P1 = X_KO - HEALTH_BAR_WIDTH - BAR_SPACING
+        X_BAR_P2 = X_KO + TEXT_KO.get_width() + BAR_SPACING
+        Y_BAR = Y_KO + 5
+
+        pygame.draw.rect(surface, RED_HEALTH, (X_BAR_P1, Y_BAR, HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT))
+        pygame.draw.rect(surface, RED_HEALTH, (X_BAR_P2, Y_BAR, HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT))
+        pygame.draw.rect(surface, YELLOW_HEALTH, (X_BAR_P1 + HEALTH_BAR_WIDTH - prog_p1, Y_BAR, prog_p1, HEALTH_BAR_HEIGHT))
+        pygame.draw.rect(surface, YELLOW_HEALTH, (X_BAR_P2, Y_BAR, prog_p2, HEALTH_BAR_HEIGHT))
+
+        self.screen.blit(TEXT_KO, (X_KO, Y_KO))
+
+    def create_world(self):
+        self.world = b2World(gravity=(0, 30), doSleep=True)
+
+        # floor (estático)
+        floor = self.world.CreateStaticBody(position=(WIDTH / 2 / PPM, (HEIGHT-60) / PPM))
+        floor.CreatePolygonFixture(box=(WIDTH / 2 / PPM, 0.5), density=0, friction=0.8)
+
+        left_wall = self.world.CreateStaticBody(
+            position=(0.25, HEIGHT / 2 / PPM),
+            shapes=b2PolygonShape(box=(0.5, HEIGHT / 2 / PPM))
+        )
+
+        right_wall = self.world.CreateStaticBody(
+            position=((WIDTH - 10) / PPM, HEIGHT / 2 / PPM),
+            shapes=b2PolygonShape(box=(0.5, HEIGHT / 2 / PPM))
+        )
+
+    def calculate_health_width(self, current_hp, max_hp, bar_width):
+        hp = max(current_hp, 0)
+        return int((hp / max_hp) * bar_width)
+    
+    def run(self):
+        choice = main_menu_screen(self.screen, self.font, MENU_OPTIONS)
+
+        if choice == "exit":
+            pygame.quit()
+            return
+
+        if choice == "play":
+            str_player, str_player2 = character_selection_screen(self.screen, self.font)
+
+            self.create_world()
+            self.background = Background()
+            self.player = Character(self.world, 100, 300, controls, name=str_player)
+            self.player2 = Character(self.world, WIDTH * 0.8, 300, controls2, name=str_player2)
+
+            FIGHTING['is_running'] = True
+
+            thread_bot = threading.Thread(target=IA_player.IA_PLAYER, daemon=True)
+            thread_timer = threading.Thread(target=timer_thread, daemon=True)
+            thread_bot.start()
+            thread_timer.start()
+
+            while FIGHTING['is_running']:
+                self.clock.tick(FPS)
+
+                self.events()
+                self.update()
+                self.draw()
+
+                if self.player.hp <= 0 or self.player2.hp <= 0:
+                    FIGHTING['is_running'] = False
+        
+        #thread_bot.join()
+        #thread_timer.join()
+
+        self.handle_victory()
+
+        self.run()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                FIGHTING['is_running'] = False
+
+        key_words = pygame.key.get_pressed()
+        key_words2 = IA_player.key_words  # IA_player.key_words  # if SINGLE_PLAYER else key_words
+
+        self.player.event_handler(key_words)
+        self.player2.event_handler(key_words2)
+
+    def update(self):
+         # Avanzar física
+        self.world.Step(1.0 / FPS, 6, 2)
+
+        # Actualizar personajes
+        self.player.update()
+        self.player2.update()
+
+        self.detect_collisions()
+
+        # Actualizar dirección de los personajes para que miren al oponente
+        self.player.update_character_direction(self.player2)
+        self.player2.update_character_direction(self.player)
+
+        self.background.update()
+
+    def detect_collisions(self):
+        # Comprobar colisiones
+        self.player.hit_check(self.player2)
+        self.player2.hit_check(self.player)
+        self.player.projectile_hit_check(self.player2)
+        self.player2.projectile_hit_check(self.player)
+
+    def draw(self):
+        # Dibujar
+        self.background.draw(self.screen)
+        self.player.draw(self.screen)
+        self.player2.draw(self.screen)
+        self.draw_health_bars(self.screen, self.player.hp, self.player2.hp)
+        draw_timer(self.screen, self.font, WIDTH)
+
+        pygame.display.flip()
+
+    def handle_victory(self):
+        winner = self.player.name if self.player.hp > 0 else self.player2.name
+        winner = 'empate' if self.player.hp == self.player2.hp else winner
+        victory_screen(self.screen, self.font, WIDTH, HEIGHT, winner)
+        pygame.display.flip()
+
+    
+if __name__ == "__main__":
+    game = Game()
+    game.run()
 
 
-clock = pygame.time.Clock()
-
-# Crear world Box2D
-world = b2World(gravity=(0, 30), doSleep=True)
-
-# floor (estático)
-floor = world.CreateStaticBody(position=(WIDTH / 2 / PPM, (HEIGHT-60) / PPM))
-floor.CreatePolygonFixture(box=(WIDTH / 2 / PPM, 0.5), density=0, friction=0.8)
-
-left_wall = world.CreateStaticBody(
-    position=(0.25, HEIGHT / 2 / PPM),
-    shapes=b2PolygonShape(box=(0.5, HEIGHT / 2 / PPM))
-)
-
-right_wall = world.CreateStaticBody(
-    position=((WIDTH - 10) / PPM, HEIGHT / 2 / PPM),
-    shapes=b2PolygonShape(box=(0.5, HEIGHT / 2 / PPM))
-)
 
 
-def calculate_health_width(current_hp, max_hp, bar_width):
-    hp = max(current_hp, 0)
-    return int((hp / max_hp) * bar_width)
 
 
-def draw_health_bars(surface, p1_healt, p2_healt):
-    # Vida actual
-    prog_p1 = calculate_health_width(p1_healt, MAX_HP, HEALTH_BAR_WIDTH)
-    prog_p2 = calculate_health_width(p2_healt, MAX_HP, HEALTH_BAR_WIDTH)
-
-    pygame.draw.rect(surface, RED_HEALTH, (X_BAR_P1, Y_BAR, HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT))
-    pygame.draw.rect(surface, RED_HEALTH, (X_BAR_P2, Y_BAR, HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT))
-    pygame.draw.rect(surface, YELLOW_HEALTH, (X_BAR_P1 + HEALTH_BAR_WIDTH - prog_p1, Y_BAR, prog_p1, HEALTH_BAR_HEIGHT))
-    pygame.draw.rect(surface, YELLOW_HEALTH, (X_BAR_P2, Y_BAR, prog_p2, HEALTH_BAR_HEIGHT))
-
-    screen.blit(TEXT_KO, (X_KO, Y_KO))
 
 
-background = Background()
-
-choice = main_menu_screen(screen, font, MENU_OPTIONS)
-
-if choice == "exit":
-    pygame.quit()
-    exit()
-if choice == "play":
-    FIGHTING['is_running'] = True
-    str_player, str_player2 = character_selection_screen(screen, font)
-    player = Character(world, 100, 300, controls, name=str_player)
-    player2 = Character(world, WIDTH * 0.8, 300, controls2, name=str_player2)
 
 
-thread_bot = threading.Thread(target=IA_player.IA_PLAYER)
-thread_timer = thread = threading.Thread(target=timer_thread)
-thread_bot.start()
-thread_timer.start()
 
-while FIGHTING['is_running']:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            FIGHTING['is_running'] = False
 
-    key_words = pygame.key.get_pressed()
-    key_words2 = key_words  # IA_player.key_words  # if SINGLE_PLAYER else key_words
 
-    player.event_handler(key_words)
-    player2.event_handler(key_words2)
 
-    # Avanzar física
-    world.Step(1.0 / FPS, 6, 2)
 
-    # Actualizar personajes
-    player.update()
-    player2.update()
 
-    # Comprobar colisiones
-    player.hit_check(player2)
-    player2.hit_check(player)
-    player.projectile_hit_check(player2)
-    player2.projectile_hit_check(player)
 
-    # Actualizar dirección de los personajes para que miren al oponente
-    player.update_character_direction(player2)
-    player2.update_character_direction(player)
 
-    # Dibujar
-    background.update()
-    background.draw(screen)
-    player.draw(screen)
-    player2.draw(screen)
-    draw_health_bars(screen, player.hp, player2.hp)
-    draw_timer(screen, font, WIDTH)
 
-    if player.hp <= 0 or player2.hp <= 0:
-        FIGHTING['is_running'] = False
 
-    pygame.display.flip()
 
-thread_bot.join()
-thread_timer.join()
-winner = player.name if player.hp > 0 else player2.name
-winner = 'empate' if player.hp == player2.hp else winner
-victory_screen(screen, font, WIDTH, HEIGHT, winner)
-pygame.display.flip()
 
-pygame.quit()
+
+
+
+
+
+
+
+  
+
+    
+
+    
+
+    
+
+    
+
+
