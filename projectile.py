21621fe17a9971f26f57f9140a3d0515aef07859
Alba <albanesigaston@gmail.com>
2025-07-02 16:30:44 -0300

    se remueven los cuerpos de los proyectiles al cambiar direccion o chocar

--- projectile.py
+++ projectile.py
@@ -2,8 +2,10 @@ import pygame
 from constants import Direction
 import math
 
+
 class Projectile:
     def __init__(self, world, x, y, direction, sprite, speed=15):
+        self.world = world
         self.sprite = sprite
         self.alive = True
         self.x = x
@@ -21,11 +23,14 @@ class Projectile:
     def update(self, screen_width_px=800, margin_px=50):
         if not self.body:
             self.alive = False
+            self.world.DestroyBody(self.body)
             return
         x_px = self.body.position.x * 30
         velocity_x = self.body.linearVelocity.x
-        if x_px < margin_px or x_px > (screen_width_px - margin_px) or abs(velocity_x) < 2:
+        velocity_y = self.body.linearVelocity.y
+        if x_px < margin_px or x_px > (screen_width_px - margin_px) or abs(velocity_x) < 2 or velocity_y != 0:
             self.alive = False
+            self.world.DestroyBody(self.body)
 
     def draw(self, screen, ppm=30):
         if not self.body:
@@ -36,7 +41,7 @@ class Projectile:
 
         angle_degrees = -math.degrees(self.body.angle)
         rotated_image = pygame.transform.rotate(self.sprite, angle_degrees)
-        
+
         rect = rotated_image.get_rect(center=(x, y))
 
         screen.blit(rotated_image, rect)
